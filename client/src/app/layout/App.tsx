d39941ba4f961fd5c2b4c84d7877b033bacd9667
VitaliyKostin <tomasevich@ukr.net>
2022-04-01 15:41:10 +0300

    1 april 2022

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import { Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import Catalog from '../../features/catalog/Catalog';
+import { Product } from '../models/Product';
+
+function App() {
+   const [products, setProducts] = useState<Product[]>([]);
+
+   useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+         .then(response => response.json())
+         .then(data => setProducts(data));
+   }, []);
+
+   function addProduct() {
+      setProducts((prevState) => [
+         ...prevState,
+         {
+            id: prevState.length + 101,
+            name: 'product' + (prevState.length + 1),
+            price: (prevState.length + 1) * 100,
+            brand: 'some adidas',
+            description: 'some description ...',
+            pictureUrl: 'http://picsum.photos/200'
+         }
+      ]);
+   }
+
+   return (
+      <>
+         <Typography variant='h1'>Re-Store</Typography>
+         <Catalog products={products} addProduct={addProduct} />
+
+      </>
+   );
+}
+
+export default App;
