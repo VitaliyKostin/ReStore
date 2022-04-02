cfb5a19cc2a6f61dddd1ee86aaf45b482240f0bf
VitaliyKostin <tomasevich@ukr.net>
2022-04-02 21:12:11 +0300

    productViewNotWork

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from '@emotion/react';
 import { Container, createTheme, CssBaseline } from '@mui/material';
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProdutcDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 
 function App() {
@@ -25,7 +30,11 @@ function App() {
          <CssBaseline />
          <Header darkMode={darkMode} themeChange={themeChange} />
          <Container>
-            <Catalog />
+            <Route exact path='/' component={HomePage} />
+            <Route path='/catalog' component={Catalog} />
+            <Route path='/catalog/:id' component={ProductDetails} />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/contact' component={ContactPage} />
          </Container>
 
 
